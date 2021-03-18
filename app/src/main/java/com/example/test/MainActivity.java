29a3d8405df3319227578c87919528de7a7b46de
favcri <favell.cristian81@gmail.com>
2021-03-18 23:23:27 +0100

    Tentative de storage avec un fichier txt. Ptêtre la bonne voie, mais ça marche pas...

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.FileUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,53 +13,116 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<String> items;
-    private ArrayAdapter<String> itemsAdapter;
-    private ListView lvItems;
+	private ArrayList<String> items;
+	private ArrayAdapter<String> itemsAdapter;
+	private ListView lvItems;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+//		setRetainInstance (true);
+		setContentView(R.layout.activity_main);
 
-        // Ajout des items dans la list
-        lvItems = (ListView) findViewById(R.id.lvItems);
-        items = new ArrayList<String>();
-        itemsAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, items);
-        lvItems.setAdapter(itemsAdapter);
-        items.add("Faire ci");
-        items.add("Faire ca");
+		// Ajout des items dans la list
+		lvItems = (ListView) findViewById(R.id.lvItems);
+		items = new ArrayList<String>();
+		readFromFile();
+		itemsAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, items);
+		lvItems.setAdapter(itemsAdapter);
+		items.add("Faire ci");
+		items.add("Faire ca");
 
-        // Setup remove listener method call
-        setupListViewListener();
-    }
+		// Setup remove listener method call
+		setupListViewListener();
+	}
 
-    public void onAddItem(View view) {
-        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
-        String itemText = etNewItem.getText().toString();
-        itemsAdapter.add(itemText);
-        etNewItem.setText("");
-    }
+	private void writeToFile() {
+		try {
+			OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput("rieder.txt", Context.MODE_PRIVATE));
+			outputStreamWriter.write(String.valueOf(items));
+			outputStreamWriter.close();
+		}
+		catch (IOException e) {
+			Log.e("Exception", "File write failed: " + e.toString());
+		}
+	}
 
-    private void setupListViewListener() {
-        lvItems.setOnItemLongClickListener(
-                new AdapterView.OnItemLongClickListener() {
-                    @Override
-                    public boolean onItemLongClick(AdapterView<?> adapter,
-                                                   View item, int pos, long id) {
-                        // Remove the item within array at position
-                        items.remove(pos);
-                        // Refresh the adapter
-                        itemsAdapter.notifyDataSetChanged();
-                        // Return true consumes the long click event (marks it handled)
-                        return true;
-                    }
+	private String readFromFile () {
+		String ret = "";
+		try {
+			InputStream inputStream = openFileInput("rieder.txt");
+			if ( inputStream != null ) {
+				InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+				BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+				String receiveString = "";
+				StringBuilder stringBuilder = new StringBuilder();
 
-                });
-    }
+				while ((receiveString = bufferedReader.readLine()) != null )
+					stringBuilder.append("\n").append(receiveString);
+				inputStream.close();
+				ret = stringBuilder.toString();
+			}
+		}
+		catch (FileNotFoundException e) {
+			Log.e("login activity", "File not found: " + e.toString());
+		} catch (IOException e) {
+			Log.e("login activity", "Can not read file: " + e.toString());
+		}
+		return ret;
+	}
+/*	private void readItems () {
+		File filesDir = getFilesDir();
+		File todoFile = new File(filesDir, "todo.txt");
+		try {
+			items = new ArrayList<String> (FileUtils.readLines (todoFile));
+		} catch (IOException e) {
+			items = new ArrayList<String>();
+		}
+	}
+
+	private void writeItems () {
+		File filesDir = getFilesDir();
+		File todoFile = new File(filesDir, "todo.txt");
+		try {
+			FileUtils.writeLines (todoFile, items);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+*/
+	private void setupListViewListener() {
+		lvItems.setOnItemLongClickListener(
+				new AdapterView.OnItemLongClickListener() {
+					@Override
+					public boolean onItemLongClick(AdapterView<?> adapter,
+												   View item, int pos, long id) {
+						// Remove the item within array at position
+						items.remove(pos);
+						// Refresh the adapter
+						itemsAdapter.notifyDataSetChanged();
+						// Return true consumes the long click event (marks it handled)
+						writeToFile();
+						return true;
+					}
+				});
+	}
+
+	public void onAddItem(View view) {
+		EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+		String itemText = etNewItem.getText().toString();
+		itemsAdapter.add(itemText);
+		etNewItem.setText("");
+		writeToFile();
+	}
 }
-
