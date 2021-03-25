72f99a1162d3084116f166f5aeae439542d78dca
favcri <favell.cristian81@gmail.com>
2021-03-25 17:27:11 +0100

    Ajout AlertDialog (pop-up) pour ajouter le Keep

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.test;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
@@ -20,6 +23,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+	private static final String TAG = "MainActivity";
 	int cpt = 0; // Pour numéroter les titres
 	private ArrayList<Keep> keeps = new ArrayList<>();
 	private KeepsAdapter keepsAdapter; // Pour formater (?) les keeps
@@ -34,11 +38,23 @@ public class MainActivity extends AppCompatActivity {
 		FloatingActionButton fab = findViewById(R.id.fab); // Bouton flottant
 		fab.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onClick(View view) { // Quand on clique sur le bouton, ça ajoute le texte dans keeps
-				EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
-				String itemText = etNewItem.getText().toString();
-				keepsAdapter.add(new Keep("Titre " + cpt++, itemText));
-				etNewItem.setText("");
+			public void onClick(View view) { // Quand on clique sur le bouton, ça ouvre un pop-up
+				final EditText taskEditText = new EditText(MainActivity.this);
+				AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
+						.setTitle("Add a new task")
+						.setMessage("What do you want to do next?")
+						.setView(taskEditText)
+						.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog, int which) { // ça met le texte dans la liste des keeps
+								String task = String.valueOf(taskEditText.getText());
+								keepsAdapter.add(new Keep("Titre " + cpt++, task));
+								Log.d(TAG, "Task to add: " + task);
+							}
+						})
+						.setNegativeButton("Cancel", null)
+						.create();
+				dialog.show();
 			}
 		});
 
