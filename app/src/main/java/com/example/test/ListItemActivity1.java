9bc141a5bf3a107af498ae75cf6e6fc555e93a62
favcri <favell.cristian81@gmail.com>
2021-03-27 20:48:36 +0100

    Test colorPicker avec un bouton qui appelle une fonction qui lance un ColorPickerDialogBuilder (fonctionne pas)

--- app/src/main/java/com/example/test/ListItemActivity1.java
+++ app/src/main/java/com/example/test/ListItemActivity1.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,43 +18,77 @@ import com.flask.colorpicker.ColorPickerView;
 import com.flask.colorpicker.OnColorSelectedListener;
 import com.flask.colorpicker.builder.ColorPickerClickListener;
 import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ListItemActivity1 extends AppCompatActivity {
+//	Button colorButton = new Button(getApplicationContext());
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_list_item1);
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate (savedInstanceState);
+		setContentView (R.layout.activity_list_item1);
+		Button colorButton = findViewById(R.id.buttonColor);
 
-		Toast toast = Toast.makeText(this, "Salam", Toast.LENGTH_SHORT);
-		toast.setMargin(50,50);
-
-		ColorPickerDialogBuilder
-				.with(this)
-				.setTitle("Choose color")
-				.initialColor(getResources().getColor(R.color.purple_200))
-				.wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
-				.density(12)
-				.setOnColorSelectedListener(new OnColorSelectedListener() {
+/*		FloatingActionButton fab = findViewById (R.id.buttonColor);
+		fab.setOnClickListener (
+				new View.OnClickListener () {
 					@Override
-					public void onColorSelected (int selectedColor) {
-						toast.setText(selectedColor);
-						toast.show();
-//						toast ("onColorSelected: 0x" + Integer.toHexString(selectedColor));
+					public void onClick (View v) {
+						ColorPickerDialogBuilder
+								.with (getApplicationContext())
+								.setTitle ("Choisissez votre couleur")
+//								.initialColor (getResources().getColor(R.color.purple_200))
+								.wheelType (ColorPickerView.WHEEL_TYPE.FLOWER)
+								.density (12)
+								.setOnColorSelectedListener (new OnColorSelectedListener() {
+									@Override
+									public void onColorSelected(int selectedColor) {}
+								})
+								.setPositiveButton ("OK", new ColorPickerClickListener() {
+									@Override
+									public void onClick (DialogInterface dialog, int selectedColor, Integer[] allColors) {
+										ConstraintLayout cl = findViewById (R.id.testColor);
+										cl.setBackgroundColor (selectedColor);
+									}
+								})
+								.setNegativeButton ("Annuler", new DialogInterface.OnClickListener() {
+									@Override
+									public void onClick (DialogInterface dialog, int which) {}
+								})
+								.build()
+								.show();
 					}
-				})
-				.setPositiveButton("ok", new ColorPickerClickListener() {
-					@Override
-					public void onClick (DialogInterface dialog, int selectedColor, Integer[] allColors) {
-						ConstraintLayout cl = findViewById (R.id.testColor);
-						cl.setBackgroundColor(selectedColor);
-					}
-				})
-				.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-					}
-				})
-				.build()
-				.show();
+				}
+		);
+*/
 	}
+
+	public void dialogColorPicker (View view) {
+			ColorPickerDialogBuilder
+					.with (getApplicationContext())
+					.setTitle ("Choisissez votre couleur")
+//								.initialColor (getResources().getColor(R.color.purple_200))
+					.wheelType (ColorPickerView.WHEEL_TYPE.FLOWER)
+					.density (12)
+					.setOnColorSelectedListener (new OnColorSelectedListener() {
+						@Override
+						public void onColorSelected(int selectedColor) {}
+					})
+					.setPositiveButton ("OK", new ColorPickerClickListener() {
+						@Override
+						public void onClick (DialogInterface dialog, int selectedColor, Integer[] allColors) {
+							ConstraintLayout cl = findViewById (R.id.testColor);
+							cl.setBackgroundColor (selectedColor);
+						}
+					})
+					.setNegativeButton ("Annuler", new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick (DialogInterface dialog, int which) {}
+					})
+					.build()
+					.show();
+//		Toast toast = Toast.makeText(this, "SALAM !", Toast.LENGTH_LONG);
+//		toast.show();
+	}
+
 }
